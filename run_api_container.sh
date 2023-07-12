2caa8a7fdbf02f3edb37f052f76eb830fa2ce28c
Ubuntu <azureuser@twvm5.ehoqgypqbbwe3cadhdhi14lbya.zx.internal.cloudapp.net>
2023-07-12 09:44:37 +0000

    fix scripts

--- run_api_container.sh
+++ run_api_container.sh
@@ -1,1 +1,1 @@
-docker run -p 8000:8000 harmonyapi
+docker run -p 8000:8000 harmonydata/harmonyapi
--- run_local_complete_harmony_container.sh
+++ run_local_complete_harmony_container.sh
@@ -1,1 +1,1 @@
-docker run -p 8000:8000 -p 3000:3000 harmonylocal
+docker run -p 8000:8000 -p 3000:3000 harmonydata/harmonylocal
