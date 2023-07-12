14c63d2c61d6cc50f1304ef4ebe830875b434893
Ubuntu <azureuser@twvm5.ehoqgypqbbwe3cadhdhi14lbya.zx.internal.cloudapp.net>
2023-07-12 08:25:13 +0000

    rename build scripts

--- build_local_harmony.sh
+++
@@ -1,2 +0,0 @@
-docker login -u harmonydata
-export COMMIT_ID=`git show -s --format=%ci_%h | sed s/[^_a-z0-9]//g | sed s/0[012]00_/_/g` && docker build -f Dockerfile_local_api -t harmonyapilocal --build-arg COMMIT_ID=$COMMIT_ID . && docker tag harmonyapilocal harmonydata/harmonyapilocal:latest && docker push harmonydata/harmonyapilocal:latest
