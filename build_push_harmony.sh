6421437420231cab87318d5d0176dbfdee0a0a47
Thomas Wood <thomas@fastdatascience.com>
2024-01-22 11:49:46 +0000

    don't have latest tag

--- build_push_harmony.sh
+++ build_push_harmony.sh
@@ -1,2 +1,2 @@
 # docker login -u harmonydata
-export COMMIT_ID=`git show -s --format=%ci_%h | sed s/[^_a-z0-9]//g | sed s/0[012]00_/_/g` && docker build -t harmonyapi -t harmonyapi:latest -t harmonyapi:$COMMIT_ID -t harmonydata/harmonyapi:latest -t harmonydata/harmonyapi:$COMMIT_ID --build-arg COMMIT_ID=$COMMIT_ID . && docker push harmonydata/harmonyapi --all-tags && echo "The container version is $COMMIT_ID"
+export COMMIT_ID=`git show -s --format=%ci_%h | sed s/[^_a-z0-9]//g | sed s/0[012]00_/_/g` && docker build -t harmonyapi -t harmonyapi:$COMMIT_ID -t harmonydata/harmonyapi -t harmonydata/harmonyapi:$COMMIT_ID --build-arg COMMIT_ID=$COMMIT_ID . && docker push harmonydata/harmonyapi:$COMMIT_ID --all-tags && echo "The container version is $COMMIT_ID"
