7664bb143e312637153fa35978fc423ade065be7
Thomas Wood <thomas@fastdatascience.com>
2024-03-14 11:47:48 +0000

    update build script

--- build_push_harmony_azure.sh
+++ build_push_harmony_azure.sh
@@ -1,4 +1,4 @@
 az login
 az acr login --name twharmonyacr
 az acr update -n twharmonyacr --admin-enabled true
-export COMMIT_ID=`git show -s --format=%ci_%h | sed s/[^_a-z0-9]//g | sed s/0[012]00_/_/g` && docker build -f Dockerfile_tika -t harmonywithtika -t harmonywithtika:latest -t harmonywithtika:$COMMIT_ID -t harmonydata/harmonywithtika:latest -t harmonydata/harmonywithtika:$COMMIT_ID -t twharmonyacr.azurecr.io/harmonywithtika -t twharmonyacr.azurecr.io/harmonywithtika:$COMMIT_ID --build-arg COMMIT_ID=$COMMIT_ID . && docker push twharmonyacr.azurecr.io/harmonywithtika:$COMMIT_ID  && echo "The container version is $COMMIT_ID" && echo "The container version is $COMMIT_ID"
+export COMMIT_ID=`git show -s --format=%ci_%h | sed s/[^_a-z0-9]//g | sed s/0[012]00_/_/g` && docker build -t harmonyapi -t harmonyapi:$COMMIT_ID -t twharmonyacr.azurecr.io/harmonyapi -t twharmonyacr.azurecr.io/harmonyapi:$COMMIT_ID --build-arg COMMIT_ID=$COMMIT_ID . && docker push twharmonyacr.azurecr.io/harmonyapi:$COMMIT_ID && echo "The container version is $COMMIT_ID"
