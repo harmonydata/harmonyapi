cba29c0cc8fb290829d96bbf084865006944eec1
Thomas Wood <thomas@fastdatascience.com>
2024-05-13 21:23:24 +0100

    Explicit directory path

--- utils/helpers.py
+++ utils/helpers.py
@@ -32,6 +32,8 @@ from typing import List
 import numpy as np
 from harmony.schemas.requests.text import Instrument, Question
 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
 
 def get_example_instruments() -> List[Instrument]:
     """Get example instruments"""
@@ -39,7 +41,7 @@ def get_example_instruments() -> List[Instrument]:
     example_instruments = []
 
     with open(
-        str(os.getcwd()) + "/example_questionnaires.json", "r", encoding="utf-8"
+            str(os.getcwd()) + "/example_questionnaires.json", "r", encoding="utf-8"
     ) as file:
         for line in file:
             instrument = Instrument.parse_raw(line)
@@ -56,21 +58,21 @@ def get_mhc_embeddings() -> tuple:
     mhc_embeddings = np.zeros((0, 0))
 
     try:
-        data_path = "mhc_embeddings" # submodule
+        data_path = os.path.join(dir_path, "../mhc_embeddings")  # submodule
         with open(
-            os.path.join(data_path, "mhc_questions.txt"), "r", encoding="utf-8"
+                os.path.join(data_path, "mhc_questions.txt"), "r", encoding="utf-8"
         ) as file:
             for line in file:
                 mhc_question = Question(question_text=line)
                 mhc_questions.append(mhc_question)
         with open(
-            os.path.join(data_path, "mhc_all_metadatas.json"), "r", encoding="utf-8"
+                os.path.join(data_path, "mhc_all_metadatas.json"), "r", encoding="utf-8"
         ) as file:
             for line in file:
                 mhc_meta = json.loads(line)
                 mhc_all_metadata.append(mhc_meta)
         with open(os.path.join(data_path, "mhc_embeddings.npy"), "rb") as file:
-            mhc_embeddings = np.load(file)
+            mhc_embeddings = np.load(file, allow_pickle=True)
     except (Exception,) as e:
         print(f"Could not load MHC embeddings {str(e)}")
 
