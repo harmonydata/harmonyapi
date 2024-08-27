51b946dce80445f52fa9fe08a89c14bcaa3e2b6c
Thomas Wood <thomas@fastdatascience.com>
2024-08-27 21:29:07 +0100

    Make Pydantic less fussy

--- tests/generate_remote_test.sh
+++ tests/generate_remote_test.sh
@@ -2,8 +2,10 @@ sed s#http://localhost:8000#https://api.harmonydata.ac.uk#g local_tests/test_par
 sed s#http://localhost:8000#https://api.harmonydata.ac.uk#g local_tests/test_match_local.py  > remote_tests/test_match_remote.py
 sed s#http://localhost:8000#https://api.harmonydata.ac.uk#g local_tests/test_match_mhc_local.py  > remote_tests/test_match_mhc_remote.py
 sed s#http://localhost:8000#https://api.harmonydata.ac.uk#g local_tests/test_different_frameworks_local.py  > remote_tests/test_different_frameworks_remote.py
+sed s#http://localhost:8000#https://api.harmonydata.ac.uk#g local_tests/test_match_local_bigger.py  > remote_tests/test_match_remote_bigger.py
 sed s#http://localhost:8000#https://harmonystagingtmp.azurewebsites.net#g local_tests/test_parse_local.py  > staging_tests/test_parse_remote.py
 sed s#http://localhost:8000#https://harmonystagingtmp.azurewebsites.net#g local_tests/test_match_local.py  > staging_tests/test_match_remote.py
 sed s#http://localhost:8000#https://harmonystagingtmp.azurewebsites.net#g local_tests/test_match_mhc_local.py  > staging_tests/test_match_mhc_remote.py
 sed s#http://localhost:8000#https://harmonystagingtmp.azurewebsites.net#g local_tests/test_different_frameworks_local.py  > staging_tests/test_different_frameworks_remote.py
+sed s#http://localhost:8000#https://harmonystagingtmp.azurewebsites.net#g local_tests/test_match_local_bigger.py  > staging_tests/test_match_remote_bigger.py
 sed s#http://localhost:3000#https://harmonydata.ac.uk#g selenium_tests/test_selenium_local.py  > selenium_tests/test_selenium_remote.py
