e0ccb99dd1fdd72c4c82bc8beba2f302e722766e
Thomas Wood <thomas@fastdatascience.com>
2025-01-25 16:14:15 +0000

    more robust code for updating submodule

--- build_push_harmony.sh
+++ build_push_harmony.sh
@@ -1,4 +1,4 @@
 # docker login -u harmonydata
-git submodule update --remote
+# cd harmony; git pull; cd ..; git add harmony; git commit -m "update submodule"; git push
 export COMMIT_ID=`git show -s --format=%ci_%h | sed s/[^_a-z0-9]//g | sed s/0[012]00_/_/g` && docker build -t harmonyapi -t harmonyapi:$COMMIT_ID -t harmonydata/harmonyapi -t harmonydata/harmonyapi:$COMMIT_ID --build-arg COMMIT_ID=$COMMIT_ID . && docker push harmonydata/harmonyapi:$COMMIT_ID && echo "The container version is $COMMIT_ID"
 # docker run -p 8000:80 harmonydata/harmonyapi:$COMMIT_ID
