1d9310f86e16d25e37b693102c5473a3268847bf
Thomas Wood <thomas@fastdatascience.com>
2025-03-11 10:58:29 +0000

    Add new winning Doxa model - this required an update of HuggingFace+Sentence Transformers to sentence-transformers==3.4.1 / huggingface-hub==0.29.3

--- harmony_api/services/hugging_face_embeddings.py
+++ harmony_api/services/hugging_face_embeddings.py
@@ -1,7 +1,8 @@
 import numpy as np
 from sentence_transformers import SentenceTransformer
 
-from harmony_api.constants import HUGGINGFACE_MINILM_L12_V2, HUGGINGFACE_MPNET_BASE_V2
+from harmony_api.constants import HUGGINGFACE_MINILM_L12_V2, HUGGINGFACE_MPNET_BASE_V2, \
+    HUGGINGFACE_MENTAL_HEALTH_HARMONISATION_1
 
 # Load Hugging Face sentence transformers
 print("INFO:\t  Checking Hugging Face models...")
@@ -11,7 +12,9 @@ model_huggingface_minilm_l12_v2 = SentenceTransformer(
 model_huggingface_mpnet_base_v2 = SentenceTransformer(
     HUGGINGFACE_MPNET_BASE_V2["model"]
 )
-
+model_huggingface_mental_health_harmonisation = SentenceTransformer(
+    HUGGINGFACE_MENTAL_HEALTH_HARMONISATION_1["model"]
+)
 
 def __get_hugging_face_embeddings(texts: list[str], model_name: str) -> np.ndarray:
     """
@@ -32,6 +35,10 @@ def __get_hugging_face_embeddings(texts: list[str], model_name: str) -> np.ndarr
         )
     elif model_name == HUGGINGFACE_MPNET_BASE_V2["model"]:
         embeddings = model_huggingface_mpnet_base_v2.encode(
+            sentences=texts, convert_to_numpy=True
+        )
+    elif model_name == HUGGINGFACE_MENTAL_HEALTH_HARMONISATION_1["model"]:
+        embeddings = model_huggingface_mental_health_harmonisation.encode(
             sentences=texts, convert_to_numpy=True
         )
 
@@ -59,4 +66,16 @@ def get_hugging_face_embeddings_minilm_l12_v2(texts: list[str]) -> np.ndarray:
 
     return __get_hugging_face_embeddings(
         texts=texts, model_name=HUGGINGFACE_MINILM_L12_V2["model"]
+    )
+
+
+def get_hugging_face_embeddings_harmonydata_mental_health_harmonisation_1(texts: list[str]) -> np.ndarray:
+    """
+    :param texts: List of texts.
+
+    Get Hugging Face embeddings.
+    """
+
+    return __get_hugging_face_embeddings(
+        texts=texts, model_name=HUGGINGFACE_MENTAL_HEALTH_HARMONISATION_1["model"]
     )
